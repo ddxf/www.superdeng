c1af84b1a52f5a3a9c1858e8881fc48d18539ff7
root <dxf1457@163.com>
2018-12-07 18:05:45 +0800

    sss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="superfang 博客">
+    <meta name="description" content="SuperFang 博客">
     <meta name="author" content="super fang<dxf1457@163.com>">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <!-- CSRF Token -->
